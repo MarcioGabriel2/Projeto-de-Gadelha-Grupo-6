ce75c62a2522ff0d2899cfab80ed079a5591efae
Eloisa Gomes <eloisagomesmt@gmail.com>
2026-04-27 21:17:34 -0300

    Adicionando a primeira versão da classe Funcionario

---
+++ Funcionario.java
@@ -0,0 +1,11 @@
+public class Funcionario {
+    private String nome;
+    private String tipo;
+    private String senha;
+
+    public Login(String nome, String tipo, String senha) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.senha = senha;
+    }
+}
