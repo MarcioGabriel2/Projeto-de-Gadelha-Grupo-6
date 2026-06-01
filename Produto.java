a8930a1e99ed097cf7f5d60804081db8401b8e18
MarcioGabriel2 <marcio.moura09455@alunos.ufersa.edu.br>
2026-05-31 21:13:17 -0300

    Update Produto.java
    Modifiquei setters/getters, incluindo validação de setters com exceptions. Também coloquei como comentário os métodos que futuramente vão para a parte de service.

--- Produto.java
+++ Produto.java
@@ -9,23 +9,26 @@ public class Produto {
     public Produto(String marca, String codigoBarras, double preco, Tipo tipo) {
         this.marca = marca;
         this.codigoBarras = codigoBarras;
-        this.preco = (preco >= 0) ? preco : 0;
+        alterarPreco(preco);    // Aproveitando validação do método
         this.tipo = tipo;
     }
     public Produto(int id, String marca, String codigoBarras, double preco, Tipo tipo) {
-        this.id = id;
+        setID(id);
         this.marca = marca;
         this.codigoBarras = codigoBarras;
-        this.preco = (preco >= 0) ? preco : 0;
+        alterarPreco(preco);    // Aproveitando validação do método
         this.tipo = tipo;
     }
-    public void alterarPreco(double preco) {
-        if (preco >= 0) this.preco = preco;
-    }
 
-    public void alterarQuantidade(double qtd) {
+    public void alterarPreco(double preco) throws RuntimeException {
+        if (preco >= 0) this.preco = preco;
+        else throw new RuntimeException ("Preço inválido!");
+    }   // Preço não pode ser negativo
+
+    public void alterarQuantidadeEstoque (double qtd) throws RuntimeException{
         if (this.quantidadeEstoque + qtd >= 0) {
-            this.quantidadeEstoque += qtd;
+            this.quantidadeEstoque += qtd;  // Aumenta o tamanho máximo da quantidade do estoque
+        else throw new RuntimeException ("Estoque inválido!");
         }
     }
 
@@ -35,6 +38,11 @@ public class Produto {
     public double getQuantidadeEstoque() { return quantidadeEstoque; }
     public String getMarca() { return marca; }
     public int getId() { return id; }
+
     // setters
-     public void setId(int id) { this.id = id; }
+    public void setId(int id) throws RuntimeException {
+        if (id >= 0) this.id = id;   // ID não pode ser negativo
+        else throw new RuntimeException("ID inválido!");
+}
+
 }
