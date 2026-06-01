362208947aaae04309a4a5fd62f9fc0558946365
MarcioGabriel2 <marcio.moura09455@alunos.ufersa.edu.br>
2026-05-31 21:14:46 -0300

    Update Tipo.java
    Modifiquei setters/getters, incluindo validação de setters com exceptions.

--- Tipo.java
+++ Tipo.java
@@ -8,15 +8,23 @@ public class Tipo {
         this.formaVenda = formaVenda;
     }
     public Tipo(int id, String nome, String formaVenda) {
-        this.id = id;
+        setId(id);
         this.nome = nome;
         this.formaVenda = formaVenda;
     }
+
+
     public void alterarFormaVenda(String f) {
         if (f != null && !f.isEmpty()) this.formaVenda = f;
     }
+
+
 // Getters e Setters necessários
     public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public void setId(int id) throws RuntimeException {
+        if (id > 0) this.id = id;   // ID não pode ser negativo
+        else throw new RuntimeException("ID inválido!");
+    }
+
     public String getNome() { return nome; }
 }
